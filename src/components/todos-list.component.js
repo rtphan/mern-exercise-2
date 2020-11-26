cfe68cbb73f3a762d4d077149153e7b96614354a
rtphan <rtphan@g.ucla.edu>
2020-11-26 21:08:26 +0000

    Initialized all components

---
+++ src/components/todos-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TodosList extends Component {
+    render() {
+        return (
+            <div>
+                <p>This is the Todos List Component!</p>
+            </div>
+        )
+    }
+}
