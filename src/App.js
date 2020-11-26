432d717607968052155406cf8ac3fcc8af6098b3
rtphan <rtphan@g.ucla.edu>
2020-11-26 21:39:21 +0000

    Created navbar component and routed all items to components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Navbar from './components/navbar.component';
+import TodosList from './components/todos-list.component';
+import EditTodo from './components/edit-todo.component';
+import CreateTodo from './components/create-todo.component';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <h2>Hello World</h2>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <Router>
+                <div className="container">
+                    <Navbar />
+                    <br/>
+                    <Route path="/" exact component={TodosList} />
+                    <Route path="/edit/:id" component={EditTodo} />
+                    <Route path="/create" component={CreateTodo} />
+                </div>
+            </Router>
+        )
+    }
 }
 
 export default App;
